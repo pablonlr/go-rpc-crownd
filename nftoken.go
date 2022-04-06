98071f6e56b9e6cecd3a5a262623d7c9e1ee58f2
pablonlr <pablonlarag@gmail.com>
2022-04-06 17:04:04 -0300

    Adding a list of nfproto and nftokens methods

---
+++ nftoken.go
@@ -0,0 +1,151 @@
+package crownd
+
+import (
+	"encoding/json"
+	"strconv"
+)
+
+//NFToken represents the information of a Non-Fungible Token in a Crown logic
+type NFToken struct {
+	ID                 string `json:"nftId"`
+	NFTProtocolID      string `json:"nftProtocolId"`
+	OwnerKeyID         string `json:"nftOwnerKeyId"`
+	MetadataAdminKeyId string `json:"metadataAdminKeyId"`
+	Metadata           string `json:"metadata"`
+	BlockHash          string `json:"blockHash"`
+	RegistrationTxHash string `json:"registrationTxHash"`
+	Height             int    `json:"height"`
+	Timestamp          uint64 `json:"timestamp"`
+}
+
+//RegisterNFToken Register a new NFT in a given protocol.
+//Creates and sends a new non-fungible token transaction
+func (client *Client) RegisterNFToken(nftProtoID string, ID string, nfTokenOwnerAddr string, metadatAdminAddr string, metadata string) (string, error) {
+	resp, err := client.Request("nftoken", "register", nftProtoID, ID, nfTokenOwnerAddr, metadatAdminAddr, metadata)
+	if resperr := parseErr(err, resp); resperr != nil {
+		return "", resperr
+	}
+	return string(resp.Result), nil
+}
+
+//ListNFTokensExplicit 	List tokens registered on the blockchain by:
+// <protocol> <owner address>, <number of elements to list>, <numb of txs to skip from tip>,<to a specific block height or "*" for current height>
+//Return a slice of NFToken struct
+func (client *Client) ListNFTokensExplicit(nftproto string, nftOwnerAddr string, count, skipFromTip int, toHeight string) ([]NFToken, error) {
+	resp, err := client.Request("nftoken", "list", nftproto, nftOwnerAddr, strconv.Itoa(count), strconv.Itoa(skipFromTip), toHeight)
+	if resperr := parseErr(err, resp); resperr != nil {
+		return nil, resperr
+	}
+	tokens := []NFToken{}
+	err = json.Unmarshal(resp.Result, &tokens)
+	if err != nil {
+		return nil, err
+	}
+	return tokens, nil
+}
+
+//ListLastNFTokens List the last registered tokens on the Crown blockchain.
+// Receives the number of tokens to list.
+//Return a slice of NFToken struct
+func (client *Client) ListLastNFTokens(count int) ([]NFToken, error) {
+	return client.ListLastNFTokensInProtocol("*", count)
+}
+
+//ListLastNFTokensInProtocol List the last registered tokens on a given protocol.
+//Receives the protocol unique ID, and the number of tokens to list.
+//Return a slice of NFToken struct
+func (client *Client) ListLastNFTokensInProtocol(nftproto string, count int) ([]NFToken, error) {
+	return client.ListLastNFTokensInProtocolOwnerBy(nftproto, "*", count)
+}
+
+//ListLastNFTokensOwnerBy List the last registered tokens owner by a given CRW address.
+//Receives the CRW owner address, and the number of tokens to list.
+//Return a slice of NFToken struct
+func (client *Client) ListLastNFTokensOwnerBy(ownerAddr string, count int) ([]NFToken, error) {
+	return client.ListLastNFTokensInProtocolOwnerBy("*", ownerAddr, count)
+}
+
+//ListLastNFTokensInProtocolOwnerBy List the last registered tokens on a given protocol, owner by a CRW address.
+//Receives the protocol unique ID, the CRW owner address and the number of tokens to list
+//Return a slice of NFToken struct
+func (client *Client) ListLastNFTokensInProtocolOwnerBy(nftproto, ownerAddr string, count int) ([]NFToken, error) {
+	return client.ListNFTokensExplicit(nftproto, ownerAddr, count, 0, "*")
+}
+
+//GetNFToken Obtain a registered NFT by a given protocol and unique NFToken ID.
+//Return a pointer to the NFToken representation.
+func (client *Client) GetNFToken(protocolID, tokenID string) (*NFToken, error) {
+	return client.getNFToken("get", protocolID, tokenID)
+}
+
+//GetNFTokenByTxID Obtain a registered NFT by the registration transaction ID.
+//Return a pointer to the NFToken representation.
+func (client *Client) GetNFTokenByTxID(txID string) (*NFToken, error) {
+	return client.getNFToken("getbytxid", txID)
+}
+
+func (client *Client) getNFToken(params ...string) (*NFToken, error) {
+	resp, err := client.Request("nftoken", params)
+	if resperr := parseErr(err, resp); resperr != nil {
+		return nil, resperr
+	}
+	token := NFToken{}
+	err = json.Unmarshal(resp.Result, &token)
+	if err != nil {
+		return nil, err
+	}
+	return &token, nil
+}
+
+//TotalSupply Number of NFTokens registered on a protocol. Require the unique NFT protocol ID.
+func (client *Client) TotalSupply(protocolID string) (int, error) {
+	resp, err := client.Request("nftoken", "totalsupply", protocolID)
+	if resperr := parseErr(err, resp); resperr != nil {
+		return -1, resperr
+	}
+	supply := 0
+	err = json.Unmarshal(resp.Result, &supply)
+	if err != nil {
+		return -1, err
+	}
+	return supply, nil
+}
+
+//BalanceOf Number of NFTokens owned by a given CRW address.
+func (client *Client) BalanceOf(address string) (int, error) {
+	return client.balanceOf(address)
+}
+
+//BalanceOf Number of NFTokens owned by a given CRW address in a specific protocol.
+//Receives the  the CRW owner address and the protocol unique ID.
+func (client *Client) BalanceOfInProto(address, nfproto string) (int, error) {
+	return client.balanceOf(address, nfproto)
+}
+
+func (client *Client) balanceOf(params ...string) (int, error) {
+	resp, err := client.Request("nftoken", "balanceof", params)
+	if resperr := parseErr(err, resp); resperr != nil {
+		return -1, resperr
+	}
+	balance := 0
+	err = json.Unmarshal(resp.Result, &balance)
+	if err != nil {
+		return -1, err
+	}
+	return balance, nil
+}
+
+//OwnerOfToken Get the CRW address owner of a given NFT.
+//Receives protocol ID and the NFToken ID.
+func (client *Client) OwnerOfToken(nfprotoID, nftokenID string) (string, error) {
+	resp, err := client.Request("nftoken", "ownerof", nfprotoID, nftokenID)
+	if resperr := parseErr(err, resp); resperr != nil {
+		return "", resperr
+	}
+	owner := ""
+	err = json.Unmarshal(resp.Result, &owner)
+	if err != nil {
+		return "", err
+	}
+	return owner, nil
+}
---
+++ nftproto.go
@@ -0,0 +1,116 @@
+package crownd
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+)
+
+//NFTProtocol represents the information of a Non-Fungible Token Protocol in a Crown logic
+type NFTProtocol struct {
+	ID                     string `json:"nftProtocolId"`
+	Name                   string `json:"tokenProtocolName"`
+	TokenProtocolOwnerAddr string `json:"tokenProtocolOwnerId"`
+	NFTRegSign             string `json:"nftRegSign"`
+	MetadataMimeType       string `json:"tokenMetadataMimeType"`
+	MetadataSchemaUri      string `json:"tokenMetadataSchemaUri"`
+	IsTokenTransferible    bool   `json:"isTokenTransferable"`
+	IsMetadataEmbedded     bool   `json:"isMetadataEmbedded"`
+	MaxMetadataSize        uint8  `json:"maxMetadataSize"`
+	BlockHash              string `json:"blockHash"`
+	RegistrationTxHash     string `json:"registrationTxHash"`
+	Height                 int    `json:"height"`
+	Timestamp              uint64 `json:"timestamp"`
+	nftRegCode             NFTSignCode
+}
+
+func (proto *NFTProtocol) NFTRegCode() NFTSignCode {
+	if proto.nftRegCode != NoValue {
+		return proto.nftRegCode
+	}
+	switch proto.NFTRegSign {
+	case fmt.Sprint(SelfSign):
+		proto.nftRegCode = SelfSign
+	case fmt.Sprint(SignByCreator):
+		proto.nftRegCode = SignByCreator
+	case fmt.Sprint(SignPayer):
+		proto.nftRegCode = SignPayer
+	}
+	return proto.nftRegCode
+}
+
+type NFTSignCode int
+
+const (
+	NoValue NFTSignCode = iota
+	SelfSign
+	SignByCreator
+	SignPayer
+)
+
+func (client *Client) RegisterNFTProtocol(ID, name, tokenProtoOwnerAddr string, nftRegSign NFTSignCode, metadataMimeType, metadataSchemaUri string, isTokenTransferible, isMetadataEmbedded bool, maxMetadataSize uint8) (string, error) {
+	resp, err := client.Request("nftproto", "register", ID, name, tokenProtoOwnerAddr, strconv.Itoa(int(nftRegSign)), metadataMimeType, metadataSchemaUri, isTokenTransferible, isMetadataEmbedded, strconv.Itoa(int(maxMetadataSize)))
+	if resperr := parseErr(err, resp); resperr != nil {
+		return "", resperr
+	}
+	return string(resp.Result), nil
+}
+
+// GET NFT Protocol
+
+func (client *Client) GetNFTProtocol(ID string) (*NFTProtocol, error) {
+	return client.getNFTProtocol("get", ID)
+}
+
+func (client *Client) GetNFTProtocolByTxID(TxID string) (*NFTProtocol, error) {
+	return client.getNFTProtocol("getbytxid", TxID)
+}
+func (client *Client) getNFTProtocol(getmethod, ID string) (*NFTProtocol, error) {
+	resp, err := client.Request("nftproto", getmethod, ID)
+	if resperr := parseErr(err, resp); resperr != nil {
+		return nil, resperr
+	}
+	getproto := &NFTProtocol{}
+	err = json.Unmarshal(resp.Result, getproto)
+	if err != nil {
+		return nil, err
+	}
+	return getproto, nil
+}
+
+//
+
+//LIST NFT Protocols
+
+func (client *Client) ListNFTProtocolsExplicit(count int, skiptxs int, toHeight string) ([]NFTProtocol, error) {
+	resp, err := client.Request("nftproto", "list", strconv.Itoa(count), strconv.Itoa(skiptxs), toHeight)
+	if resperr := parseErr(err, resp); resperr != nil {
+		return nil, resperr
+	}
+	protos := []NFTProtocol{}
+	err = json.Unmarshal(resp.Result, &protos)
+	if err != nil {
+		return nil, err
+	}
+	return protos, nil
+
+}
+func (client *Client) ListNFTProtocolsToBlockN(count int, toBlockHeight int) ([]NFTProtocol, error) {
+	return client.ListNFTProtocolsExplicit(count, 0, strconv.Itoa(toBlockHeight))
+}
+
+func (client *Client) ListLastNFTProtocols(count int) ([]NFTProtocol, error) {
+	return client.ListNFTProtocolsExplicit(count, 0, "*")
+}
+
+//
+
+func (client *Client) OwnerOfProto(nftProtoID string) (string, error) {
+	resp, err := client.Request("nftproto", "ownerof", nftProtoID)
+	if resperr := parseErr(err, resp); resperr != nil {
+		return "", resperr
+	}
+	var s string
+	err = json.Unmarshal(resp.Result, &s)
+	return s, nil
+}
