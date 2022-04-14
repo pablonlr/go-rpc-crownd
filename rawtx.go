5ea5d55940c0ee8653be30489a08995106deda6f
pablonlr <pablonlarag@gmail.com>
2022-04-14 20:35:01 -0300

    GetRawTx and DecodeRawTx methods

--- rawtx.go
+++ rawtx.go
@@ -2,6 +2,39 @@ package crownd
 
 import "encoding/json"
 
+type GetRawTxResponse struct {
+	TxID     string             `json:"txid"`
+	Version  int                `json:"version"`
+	Locktime int                `json:"locktime"`
+	Vin      []*TxInputResponse `json:"vin"`
+}
+
+type TxInputResponse struct {
+	TxID      string             `json:"txid"`
+	Vout      int                `json:"vout"`
+	ScriptSig *ScriptSigResponse `json:"scriptSig"`
+	Sequence  int                `json:"sequence"`
+}
+
+type ScriptSigResponse struct {
+	Asm string `json:"asm"`
+	Hex string `json:"hex"`
+}
+
+type TxOutputResponse struct {
+	Value       float64         `json:"value"`
+	N           int             `json:"n"`
+	SriptPubKey *TxOutputScript `json:"scriptPubKey"`
+}
+
+type TxOutputScript struct {
+	Asm       string   `json:"asm"`
+	Hex       string   `json:"hex"`
+	ReqSigs   int      `json:"reqSigs"`
+	Type      string   `json:"type"`
+	Addresses []string `json:"addresses"`
+}
+
 func (client *Client) SendRawTx(txHex string) (string, error) {
 	resp, err := client.Request("sendrawtransaction", txHex)
 	if resperr := parseErr(err, resp); resperr != nil {
@@ -10,4 +43,25 @@ func (client *Client) SendRawTx(txHex string) (string, error) {
 	var s string
 	err = json.Unmarshal(resp.Result, &s)
 	return s, nil
+}
+
+func (client *Client) GetRawTransaction(txid string) (string, error) {
+	resp, err := client.Request("getrawtransaction", txid)
+	if resperr := parseErr(err, resp); resperr != nil {
+		return "", resperr
+	}
+	var hex string
+	err = json.Unmarshal(resp.Result, &hex)
+	return hex, nil
+}
+
+func (client *Client) DecodeRawTransaction(hex string) (*GetRawTxResponse, error) {
+	resp, err := client.Request("decoderawtransaction", hex)
+	if resperr := parseErr(err, resp); resperr != nil {
+		return nil, resperr
+	}
+	var tx *GetRawTxResponse
+	err = json.Unmarshal(resp.Result, tx)
+	return tx, nil
+
 }
